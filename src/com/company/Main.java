050766e09302fe56fd4b268366e3ef70f2ba6239
Theodoros Vasileiadis <theo.vasileiadis.work@gmail.com>
2016-11-17 15:02:25 +0200

    Changed message shown

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("I am happy to meet you");
+
     }
 }
